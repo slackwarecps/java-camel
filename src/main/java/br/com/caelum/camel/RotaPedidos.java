098fdd8427cb89a0cada6d4cb46ffe7a50b8010b
faapereira <faapereira@faapereira10.indra.es>
2020-06-22 16:20:30 -0300

    Acessando o post no camel

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -9,42 +10,26 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-		
-		//Adicionar uma Rota
-		context.addRoutes(new RouteBuilder() { //cuidado, não é RoutesBuilder
+		context.addRoutes(new RouteBuilder() {
 
-		    @Override
-		    public void configure() throws Exception {
-		    	//Aqui que configuramos a rota..
-		    	
-		    	
-		    	System.out.println("roudou!!");
-		    	//busca de 5 em cinco segundos na pasta
-		    	//noop=true nao apaga os arquivos na origem
-		    	//marshal() transforma mensagem
-		    	
-		    	from("file:pedidos?delay=5s&noop=true").
-			    	split().
-			    		xpath("/pedido/itens/item").
-			    		log("${body}").
-			    	filter().
-			    		xpath("/item/formato[text()='EBOOK']").
-				    	//  log("${exchange.pattern}"). 
-				    	//  log("${id} - ${body}").
-				    	log("${body}").
-			    	marshal().xmljson().
-			    	setHeader("CamelFileName",simple("${file:name.noext}.json")).
-		    	to("file:saida");
-		    	
-		    	
-		    }
+			@Override
+			public void configure() throws Exception {
+
+				from("file:pedidos?delay=5s&noop=true").
+					split().
+						xpath("/pedido/itens/item").
+					filter().
+						xpath("/item/formato[text()='EBOOK']").
+					marshal().xmljson().
+					log("${id} - ${body}").
+					setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+				to("file:saida");
+			}
+			
 		});
-		System.out.println("iniciando...");
-	    context.start();
-	    System.out.println("aguardando...");
-	    Thread.sleep(5000);
-	    System.out.println("Finalizando...");
-	    context.stop();
-	    
-	}
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}	
 }
